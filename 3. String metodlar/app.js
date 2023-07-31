971ca53303d8d0702dbb47815118f4ff86adec9f
ReactItGrup <140500952+ReactItGrup@users.noreply.github.com>
2023-07-31 15:47:37 +0500

    Add files via upload

---
+++ 3. String metodlar/app.js
@@ -0,0 +1,66 @@
+// string
+console.log("Hello world q1234 #$%%^!");
+
+let email = "atursunaliyv@gmail.com";
+console.log(email);
+
+// string concatenation
+let firstName = "Asilbek";
+let lastName = "Tursunaliyev";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length => uzunlik
+console.log(fullName.length);
+
+// string methods => funktsiyaga o'xshash ish bajaradi
+console.log(fullName.toUpperCase());
+
+// let result = fullName.toLowerCase();
+// console.log(result);
+
+let index = email.indexOf("@");
+console.log(index);
+
+// lastIndeOf()
+// let result = email.lastIndexOf("o");
+// console.log(result);
+
+// indexOf kiritilgan malumotni boshidan boshlab qidirsa
+// lastIndexOff shuni teskarisi yani malumotni oxiridan boshlab tekshiradi
+
+// slice()
+/*;('asilbek@gmail.com')
+let result = email.slice(10)
+console.log(result) */
+
+// slice ni biz malumotlarni kesishda ishlatamiz
+
+// substr();
+// let result = email.substr(1, 9).length;
+// console.log(result); 3201
+
+// replace()
+// let result = email.replace("a", "h");
+// console.log(result);
+
+// charAt() - birinchi harakterni olib beradi
+// let result = email.charAt()
+// console.log(result)
+
+// split()
+// let result = email.split('a')
+// console.log(result)
+
+// Yozganimizni array qilib chiqazib beradi
+
+// trim()
+
+// let result = email.trim();
+// console.log(result.length, email.length);
+
+// ortiqcha ochilgan joylarni yo'q qilib chiqazib beradi

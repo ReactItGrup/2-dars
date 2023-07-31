bed347e8d727db24918a0645683a9aee18a521a0
ReactItGrup <140500952+ReactItGrup@users.noreply.github.com>
2023-07-31 15:46:28 +0500

    Add files via upload

---
+++ 2. String/script.js
@@ -0,0 +1,18 @@
+// string
+console.log("Hello world q1234 #$%%^!");
+
+let email = "atursunaliyev@gmail.com";
+console.log(email);
+
+// string concatenation
+let firstName = "Asilbek";
+let lastName = "Tursunaliyev";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
